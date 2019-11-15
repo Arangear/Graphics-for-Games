e4dc4e1b468f137e5d141362477327e20405f652
Daniel Cieslowski <D.Cieslowski2@newcastle.ac.uk>
2019-11-15 14:17:40 +0000

    Imported nclgl

---
+++ Graphics for Games/nclgl/Frustum.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "Plane.h"
+#include "Matrix4.h"
+#include "SceneNode.h"
+class Matrix4;
+
+class Frustum 
+{
+public:
+	Frustum(void) {};
+	~Frustum(void) {};
+	void FromMatrix(const Matrix4& mvp);
+	bool InsideFrustum(SceneNode& n);
+protected:
+	Plane planes[6];
+};
